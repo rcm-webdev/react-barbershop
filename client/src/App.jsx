d879e713aeb32e29abcc31894c6fe722131ee4af
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-08-07 15:30:59 -0700

    add tailwindcss and daisyui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 //Create a global header for all pages on the site, and render the target of the clicked route as the body of the page.
 
 const App = () => {
+  const links = [
+    { to: "/", label: "Home" },
+    { to: "/staff", label: "Staff" },
+    { to: "/services", label: "Services" },
+    { to: "/booking", label: "Booking" },
+    { to: "/admin", label: "Admin" },
+  ];
   return (
     <div>
       <header>
         <h1>Local Legendz Barbershop</h1>
-        <nav>
-          <a href="/">Home</a> | <a href="/staff">Staff</a> |{" "}
-          <a href="/services">Services</a> | <a href="/booking">Booking</a> |{" "}
-          <a href="/admin">Admin</a>
+        <nav className="flex flex-row gap-3">
+          {links.map((link) => (
+            <Link key={link.to} to={link.to} className="btn">
+              {link.label}
+            </Link>
+          ))}
         </nav>
       </header>
       <main>
