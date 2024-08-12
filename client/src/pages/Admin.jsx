5ddf3d81d7b69f543875937cfdc43c075cc607a0
rcm-webdev <rcm.webdev.pro@gmail.com>
2024-08-12 16:26:19 -0700

    create admin table

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -15,16 +15,78 @@ const Admin = () => {
 
   return (
     <div>
-      <h1>Admin Dashboard</h1>
-      <ul>
-        {bookings.map((booking) => (
-          <li key={booking._id}>
-            {booking.name} - {booking.email} -{" "}
-            {new Date(booking.date).toLocaleDateString()} - {booking.service} -{" "}
-            {booking.barber}
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-2xl font-bold mb-4">Admin Dashboard</h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Service</th>
+              <th>Barber</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => (
+              <tr key={booking._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={`https://ui-avatars.com/api/?name=${booking.name}&background=random`}
+                          alt="Avatar"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{booking.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{booking.email}</td>
+                <td>{new Date(booking.date).toLocaleDateString()}</td>
+                <td>
+                  {booking.service}
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    {booking.barber}
+                  </span>
+                </td>
+                <td>{booking.barber}</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+          {/* foot */}
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Service</th>
+              <th>Barber</th>
+              <th></th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </div>
   );
 };
